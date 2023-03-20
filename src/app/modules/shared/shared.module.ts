7a80ced8461e8870d1002a5778b8e9e819202101
GersonVis <visoso126@gmail.com>
2023-03-19 19:21:10 -0600

    creando toolbar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
     SidenavComponent,
+    ToolbarComponent,
   ],
   exports:[
-    SidenavComponent
+    SidenavComponent,
+    MaterialModule
   ],
   imports: [
     CommonModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    
   ]
 })
 export class SharedModule { }
