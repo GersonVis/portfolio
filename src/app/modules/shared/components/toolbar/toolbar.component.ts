2738eef29fcd94bfb769f46b578961ddfedd30bc
GersonVis <visoso126@gmail.com>
2023-03-22 23:05:54 -0600

    reparando interfaz

--- src/app/modules/shared/components/toolbar/toolbar.component.ts
+++ src/app/modules/shared/components/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import {menuOptions, MenuOption} from "../../constans/constantes-navegacion"
+
 
 @Component({
   selector: 'app-toolbar',
@@ -7,16 +8,36 @@ import {menuOptions, MenuOption} from "../../constans/constantes-navegacion"
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit{
+  
+
   menuOptions: any
   anterior: any = null
-  imOn: MenuOption 
+  imOn: MenuOption
+  evtToolbarClose: any = null
+  contentOption: any = {} 
   @Input() optionSelect=""
-  constructor(){
+
+
+
+  varAnimation: any={
+    toolbarHide: false,
+    timeHide: 450 
+  }
+  constructor(private renderer: Renderer2){
     this.menuOptions=menuOptions
     this.imOn=menuOptions['inicio']
+    
+  }
+  ngOnInit(): void {
+    this.menuOptions[this.optionSelect].active=true
+    this.imOn=menuOptions[this.optionSelect]
+    this.contentOption["imgSelected"]=this.imOn.img
+    this.contentOption["imgTitle"]=this.imOn.img
+    this.contentOption["imgInfo"]=this.imOn.img
   }
   selectOption(option: string){
     this.menuOptions[option].active=true
+    
   }
   toolbarStyle={
     "min-height": "100px",
@@ -28,16 +49,32 @@ export class ToolbarComponent implements OnInit{
   }
   toolbarBaseVisible:boolean = true
   
-  ngOnInit(): void {
-    this.menuOptions[this.optionSelect].active=true
-    this.imOn=menuOptions[this.optionSelect]
-  }
+  
   
   toolbarOver(event: any){
+    this.showToolbar()
+  }
+  toolbarOut(event: any, toolBar: any){
+    let item: any = event.currentTarget
+
+    console.log(item.clientHeight)
+    let height: number = parseInt(item.clientHeight)
+    this.hiddenToolbar(item, height)
+  }
+  showToolbar(){
+    this.varAnimation.toolbarHide=false
     this.toolbarBaseVisible=false
   }
-  toolbarOut(event: any){
-    this.toolbarBaseVisible=true
+  hiddenToolbar(toolBar: any, height: number){
+    let main=this
+    this.varAnimation.toolbarHide=true
+    toolBar.style.height=height+"px"
+    if(this.evtToolbarClose){
+      clearTimeout(this.evtToolbarClose)
+    }
+    this.evtToolbarClose=setTimeout(function(){
+      main.toolbarBaseVisible=true
+    }, this.varAnimation.timeHide)
   }
   optionClick(event: any){
     let item:any = event.currentTarget
@@ -47,6 +84,9 @@ export class ToolbarComponent implements OnInit{
     }
     this.anterior=item
     
+  }
+  overOption($event: any){
+
   }
   
 }
